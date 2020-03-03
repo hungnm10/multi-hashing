17ac02fe133366dc5d48fcce51c18490233caedb
hungnm <hungnm@cbex.xyz>
2020-03-03 17:39:41 +0700

    add curvehash

---
+++ curvehash.h
@@ -0,0 +1,16 @@
+#ifndef CURVEHASH_H
+#define CURVEHASH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void curve_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
